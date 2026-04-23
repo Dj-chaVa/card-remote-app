983486c406c3a74be61ed09dd6199d4e111a6111
Dj-chaVa <st917oraclejewel@icloud.com>
2026-04-23 14:42:02 +0900

    Add files via upload

---
+++ js-weather-app/main.js
@@ -0,0 +1,33 @@
+const weatherForm=document.getElementById("weather-form")
+const cityInput=document.getElementById("city-input")
+const weatherResults=document.getElementById("weather-results")
+
+weatherForm.addEventListener("submit",(e)=>{
+  
+  e.preventDefault()
+  getWeather(cityInput.value)
+
+
+})
+
+
+
+const getWeather=(city)=>{
+  fetch(`https://api.weatherapi.com/v1/current.json?key=8121ae91fd9a4d0185822410260204&q=${city}&aqi=no`)
+.then(response=>response.json())
+   .then(jsonData=>weatherResults.innerHTML=`
+      <div class="results-country">${jsonData.location.country}</div>
+                    <div class="results-city">${jsonData.location.name}</div>
+                    <div class="results-temp">${jsonData.current.temp_c}<span>°C</span></div>
+                    <div class="results-condition">
+                      <img src="https:${jsonData.current.condition.icon}"alt="icon">
+                      <span>${jsonData.current.condition.text}</span>
+                    </div>
+     ` 
+    )
+   }
+   
+   
+
+
+
